76838ed98ee4d65092112f91bd1c34502122674c
Ab0907 <109592962+Ab0907@users.noreply.github.com>
2023-09-14 17:45:22 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 openai.api_key = ''
 model_name = 'gpt-3.5-turbo'
 generic_prompt = """
->>>I have an ITaskHandler interface that implements handler function which is responsible for generating fixed output if the request matches certain requirements. ITaskHandler is implemented by classes AdditionTask, MultiplyTask, DivideTask. Client calls ITaskHandler through a chain of responsibility design pattern. <<<
+>>><input_text><<<
 
 ======
 Intsuction prompt
@@ -52,17 +52,21 @@ ServiceA -> Device : Send Command
 @enduml
 \"\"\"
 }
-
 """
 uml_text_file_path = "C:\Hack23\ChatDesignDoc\plantUml.txt"
 uml_img_file_path = "C:\Hack23\ChatDesignDoc\plantUml.png"
 doc_path = 'C:\Hack23\ChatDesignDoc\chatgptdoc.docx'
-blob_service_client = BlobServiceClient.from_connection_string("BlobEndpoint=https://testingaci.blob.core.windows.net/;QueueEndpoint=https://testingaci.queue.core.windows.net/;FileEndpoint=https://testingaci.file.core.windows.net/;TableEndpoint=https://testingaci.table.core.windows.net/;SharedAccessSignature=sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-09-14T02:57:58Z&st=2023-09-13T18:57:58Z&spr=https&sig=2bZnkYpf1n36QdZYB3S3hH5Wb3aVJ%2B8rZGsvfIp28Ws%3D")
+blob_service_client = BlobServiceClient.from_connection_string("BlobEndpoint=https://testingaci.blob.core.windows.net/;QueueEndpoint=https://testingaci.queue.core.windows.net/;FileEndpoint=https://testingaci.file.core.windows.net/;TableEndpoint=https://testingaci.table.core.windows.net/;SharedAccessSignature=sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-01-25T02:57:58Z&st=2023-09-13T18:57:58Z&spr=https&sig=66XZYbaPFUROK4CI%2BAWP1%2BRghH%2BsfQKawjlnLoiZS98%3D")
 container_name = 'hackchat'
 
 # Functions
-def get_completion(input_text):
-    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": input_text}])
+
+def replace_input_text(input_string, template=generic_prompt):
+    return template.replace("<input_text>", input_string)
+
+def get_completion(template, input_text):
+    input_text_in_template = replace_input_text(input_text, template)
+    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": input_text_in_template}])
     return chat_completion.choices[0].message.content
 
 def Convert_string_to_json(st):
@@ -120,8 +124,9 @@ app = Flask(__name__)
 @app.route('/gpt', methods=['GET'])
 def gpt():
     text_input = request.args.get('text_input')
+    # text_input = "I have an application with interface IService. IService is implemented by multiple services in the application and is responsible for taking a json file input from a location provided by the caller and the output is a deserialized object of the json file. Write a design doc to extend the interface for a new service A. Use singleton to implement the same."
     # Add Open Ai Key before running get_completion.
-    output_gpt = get_completion(text_input)
+    output_gpt = get_completion(generic_prompt, text_input)
     # Below string is a sample output from GPT-3. This can be used to text other methods. Uncomment the below line and comment the above line for that.
     # output_gpt = '{\n\"DesignDoc\": {\n  \"Intent\": \"The intent of the design is to implement a chain of responsibility design pattern to handle different types of tasks based on specific requirements.\",\n  \"Pros\": [\n    \"Flexibility: The chain of responsibility design pattern allows adding or removing responsibilities dynamically at runtime.\",\n    \"Decoupling: The pattern decouples the sender and receiver of a request, allowing them to vary independently.\",\n    \"Simplification: It simplifies the client code by abstracting away the details of the handling logic and providing a unified interface.\"\n  ],\n  \"Cons\": [\n    \"Performance impact: The chain of responsibility may have performance implications as each handler needs to check if it can handle the request, potentially leading to multiple iterations.\",\n    \"Complexity: The pattern can introduce additional complexity, especially when the number of handlers increases, making it harder to maintain and debug.\"\n  ]\n},\n\"Diagram\": \"\"\"\n@startuml\nclass Client\n\ninterface ITaskHandler {\n  +handler()\n}\n\nclass AdditionTask {\n  +handler()\n}\n\nclass MultiplyTask {\n  +handler()\n}\n\nclass DivideTask {\n  +handler()\n}\n\nClient --> ITaskHandler\nITaskHandler <|- AdditionTask\nITaskHandler <|- MultiplyTask\nITaskHandler <|- DivideTask\n\n@enduml\n\"\"\"\n}'
     json_output_gpt = Convert_string_to_json(output_gpt)
@@ -131,7 +136,7 @@ def gpt():
     output_doc_blob_link = add_file_to_blob(doc_path, generate_random_string(10)+".docx", container_name)
     output_img_blob_link = add_file_to_blob(uml_img_file_path, generate_random_string(10)+".png", container_name)
     print(output_doc_blob_link)
-    response = {'document': output_doc_blob_link, 'diagram' : output_img_blob_link}
+    response = {'document': output_doc_blob_link, 'diagram' : output_img_blob_link, 'input_text': text_input, 'output_gpt': output_gpt}
     return jsonify(response)
 
 if __name__ == '__main__':
